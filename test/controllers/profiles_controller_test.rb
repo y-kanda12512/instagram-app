e1659d00d79d3274e9c962dfd64f4737fb7464d8
y-kanda12512 <149121964+y-kanda12512@users.noreply.github.com>
2025-09-07 11:46:31 +0900

    ヘルパー修正

--- test/controllers/profiles_controller_test.rb
+++ test/controllers/profiles_controller_test.rb
@@ -2,22 +2,22 @@ require "test_helper"
 
 class ProfilesControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get profiles_new_url
+    get profile_new_url
     assert_response :success
   end
 
   test "should get show" do
-    get profiles_show_url
+    get profile_show_url
     assert_response :success
   end
 
   test "should get edit" do
-    get profiles_edit_url
+    get profile_edit_url
     assert_response :success
   end
 
   test "should get update" do
-    get profiles_update_url
+    get profile_update_url
     assert_response :success
   end
 
